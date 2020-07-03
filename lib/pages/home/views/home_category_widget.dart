29805d3fe5be92aa1271ae346be430194fd766cb
LeannaLady <1351993796@qq.com>
2020-07-03 08:33:22 +0800

    screen_unit

--- lib/pages/home/views/home_category_widget.dart
+++ lib/pages/home/views/home_category_widget.dart
@@ -14,26 +14,26 @@ class HomeCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List categoryList = Provider.of<HomeContentProvider>(context, listen: false).categoryList;
+    List categoryList =
+        Provider.of<HomeContentProvider>(context, listen: false).categoryList;
     if (categoryList.length > 10) {
       categoryList.removeRange(10, categoryList.length);
     }
     return Consumer<HomeContentProvider>(
-      builder: (BuildContext context, value, child) {
-        return Container(
-          height: 165,
-          padding: EdgeInsets.all(ScreenUtil().setWidth(3.0)),
-          child: GridView.count(
-            physics: const NeverScrollableScrollPhysics(),
-            crossAxisCount: 5,
-            padding: EdgeInsets.all(5.0),
-            children: categoryList.map((item) {
-              return _categoryItem(context, item);
-            }).toList(),
-          ),
-        );
-      }
-    );
+        builder: (BuildContext context, value, child) {
+      return Container(
+        height: 165,
+        padding: EdgeInsets.all(ScreenUtil().setWidth(3.0)),
+        child: GridView.count(
+          physics: const NeverScrollableScrollPhysics(),
+          crossAxisCount: 5,
+          padding: EdgeInsets.all(5.0),
+          children: categoryList.map((item) {
+            return _categoryItem(context, item);
+          }).toList(),
+        ),
+      );
+    });
   }
 }
 
@@ -44,7 +44,7 @@ Widget _categoryItem(BuildContext context, item) {
     },
     child: Column(
       children: <Widget>[
-        ImageWidget(url: item['image'], w:ScreenUtil().setWidth(95)),
+        ImageWidget(url: item['image'], w: ScreenUtil().setWidth(95)),
         // Image.network(item['image'], width: ScreenUtil().setWidth(95)),
         Text(
           item['mallCategoryName'],
